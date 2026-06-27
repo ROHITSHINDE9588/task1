4bc58e510d4652965e1d0339feadeffe091d1677
ROHITSHINDE9588 <rohitshinde958869@gmail.com>
2026-06-27 13:03:54 +0530

    1st

---
+++ 1st.py
@@ -0,0 +1,13 @@
+# My name
+name = "Rohit Jayram Shinde"
+
+# My age
+age = 18  # I'm relatively new in the world!
+
+# My favorite game
+favorite_game = "Forza Horizon 5"
+
+# Display the information
+print("Name:", name)
+print("Age:", age)
+print("Favorite Game:", favorite_game)
